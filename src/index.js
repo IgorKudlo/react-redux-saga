bd2c407bdd561188074ca813da82a86f95985179
Igor Kudlo <igoreklida@gmail.com>
2021-02-18 11:06:11 +0300

    feat: add Provider

--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from "redux";
 import {rootReducer} from "./redux/rootReducer";
+import {Provider} from "react-redux"
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 
 const store = createStore(rootReducer)
 
+const app = (
+    <Provider store={store}>
+        <React.StrictMode>
+            <App/>
+        </React.StrictMode>
+    </Provider>
+)
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    app,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
