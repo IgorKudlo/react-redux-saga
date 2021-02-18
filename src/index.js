11485fd159818815d0210dfff571b9d30ec355da
Igor Kudlo <igoreklida@gmail.com>
2021-02-18 17:22:51 +0300

    feat: add thunk, work with store over hooks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {compose, createStore} from "redux";
-import {rootReducer} from "./redux/rootReducer";
+import {applyMiddleware, compose, createStore} from "redux";
 import {Provider} from "react-redux"
+import thunk from "redux-thunk";
+import {rootReducer} from "./redux/rootReducer";
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 
 const store = createStore(rootReducer, compose(
+    applyMiddleware(
+        thunk
+    ),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ))
 
