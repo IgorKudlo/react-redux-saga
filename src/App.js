8176630a162ab5fdc4364a7b49ff7dc8b4ebe6cb
Igor Kudlo <igoreklida@gmail.com>
2021-02-18 16:39:11 +0300

    feat: add connect in component

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           <div className="row">
               <div className="col">
                   <h2>Синхронные Посты</h2>
-                  <Posts posts={[1, 2, 3]} />
+                  <Posts />
               </div>
               <div className="col">
                   <h2>Асинхронные Посты</h2>
